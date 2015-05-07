1082379ebd6ded3e79e0b2e24ea122b1a0eabf37
Muthoni <muthoni.nguthi@betterworldsystems.co.ke>
2015-05-07 13:11:40 +0300

    Added Models: Patient , Drugs

--- src/main/java/com/springapp/model/Drugs.java
+++ src/main/java/com/springapp/model/Drugs.java
@@ -3,5 +3,43 @@ package com.springapp.model;
 /**
  * Created by dhis on 07/05/15.
  */
+
+@Entity
+@Table(name="drugs")
 public class Drugs {
+    @Id
+    @GeneratedValue
+    private int DId;
+
+    @Column(name = "drug_name")
+    private String dname;
+
+    private int PId;
+
+    private String phone;
+
+    public int getDId() {
+        return DId;
+    }
+
+    public void setDId(int DId) {
+        this.DId = DId;
+
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+    public int getPId() {
+        return PId;
+    }
+
+    public void setPId(int PId) {
+        this.PId = PId;
+    }
+}
 }
--- src/main/java/com/springapp/model/Patient.java
+++ src/main/java/com/springapp/model/Patient.java
@@ -1,7 +1,72 @@
 package com.springapp.model;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 /**
  * Created by dhis on 07/05/15.
  */
-public class Patient {
+
+
+
+    @Entity
+    @Table(name="employee")
+    public class Patient {
+        @Id
+        @GeneratedValue
+        private int PId;
+
+        @Column(name = "first_name")
+        private String first_name;
+
+        private String last_name;
+
+        private Date dob;
+
+        private Timestamp timeEntry;
+
+        public int getPId() {
+            return PId;
+        }
+
+        public void setPId(int PId) {
+            this.PId = PId;
+        }
+
+        public String getFirst_name() {
+            return first_name;
+        }
+
+        public void setFirst_name(String first_name) {
+            this.first_name = first_name;
+        }
+
+        public String getLast_name() {
+            return last_name;
+        }
+
+        public void setLast_name(String last_name) {
+            this.last_name = last_name;
+        }
+
+        public Date getDate() {
+            return dob;
+        }
+
+        public void setDate(Date dob) {
+            this.dob = dob;
+        }
+
+        public Timestamp getTimeEntry() {
+            return timeEntry;
+        }
+
+        public void setTimeEntry(Timestamp timeEntry) {
+            this.timeEntry = timeEntry;
+        }
+
+
 }
+
+
+
